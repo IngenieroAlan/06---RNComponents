62d837778e9bff03e96ac6d153a2d78e0895c431
IngenieroAlan <brandon.rod182@gmail.com>
2023-07-05 19:10:45 -0700

    Alert Prompt commit

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import { Button, View, Alert } from 'react-native'
-import { HeaderTitle } from '../components/HeaderTitle'
-import { styles } from '../theme/appTheme'
+//REACT
+import React from 'react';
+import { Button, View, Alert } from 'react-native';
+//Terceros
+import prompt from 'react-native-prompt-android';
+//Nuestro
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+
 
 export const AlertScreen = () => {
-    const showAlert = () => {
-        Alert.alert('Titulo', 'Mensaje de la alerta', [
-            {
-              text: 'Cancelar',
-              onPress: () => console.log('Cancel Pressed'),
-              style: 'destructive',
-            },
-            {text: 'Aceptar', onPress: () => console.log('OK Pressed')},
-          ],{
-            cancelable: true,
-            onDismiss: () => console.log('Dismissed'),
-          });      
-    }
-    return (
-        <View style={styles.globalMargin}>
-            <HeaderTitle title='Alerts' />
-            <Button
-                title='Mostrar alerta'
-                onPress={showAlert}
-            />
-        </View>
-    )
+  const showAlert = () => {
+    Alert.alert('Titulo', 'Mensaje de la alerta', [
+      {
+        text: 'Cancelar',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'destructive',
+      },
+      { text: 'Aceptar', onPress: () => console.log('OK Pressed') },
+    ], {
+      cancelable: true,
+      onDismiss: () => console.log('Dismissed'),
+    });
+  }
+  const promptAlert = () => {
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: '',
+        placeholder: 'Constraseña',
+      }
+    );
+  }
+  return (
+    <View style={styles.globalMargin}>
+      <HeaderTitle title='Alerts' />
+      <Button
+        title='Mostrar alerta'
+        onPress={showAlert}
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        title='Mostrar prompt'
+        onPress={promptAlert}
+      />
+    </View>
+  )
 }
