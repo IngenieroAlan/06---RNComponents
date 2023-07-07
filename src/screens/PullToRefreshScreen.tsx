014ec0c58a5130389b059b583e0801833d7c7cf1
IngenieroAlan <brandon.rod182@gmail.com>
2023-07-06 18:31:13 -0700

    Custom PullToRef commit

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,32 +1,49 @@
 import React, { useState } from 'react'
-import { ScrollView, View, RefreshControl } from 'react-native'
+import { ScrollView, View, RefreshControl, Text } from 'react-native'
 import { HeaderTitle } from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export const PullToRefreshScreen = () => {
-  const [refreshing, setRefreshing] = useState(false);
-  const onRefresh = () =>{
-    setRefreshing(true);
-    setTimeout(()=>{
-        console.log('Refreshing');
-        setRefreshing(false);
-    },1000);
-  }  
-  return (
-    <ScrollView
-        refreshControl={
-            <RefreshControl
-                refreshing={refreshing}
-                onRefresh={onRefresh}
-            />
-        }
-    >
-        <View style={styles.globalMargin}>
-            <HeaderTitle
-                title='Pull to refresh'
-            />
+    const { top } = useSafeAreaInsets();
+    const [refreshing, setRefreshing] = useState(false);
+    const [data, setData] = useState<String>();
+    const onRefresh = () => {
+        setRefreshing(true);
+        setTimeout(() => {
+            console.log('Refreshing');
+            setRefreshing(false);
+            setData("Hola mundo");
+        }, 1500);
+    }
+    return (
+        <ScrollView
+            style={{ marginTop: refreshing ? top : 0 }}
+            refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    progressViewOffset={10}
+                    progressBackgroundColor="white"
+                    colors={['red', 'green', 'yellow']}
+                /* style={{ backgroundColor: "#5856D6" }}
+                tintColor="white" */
+                /* title='Refreshing...'
+                titleColor="white" */
 
-        </View>
-    </ScrollView>
-  )
+                />
+            }
+        >
+            <View style={styles.globalMargin}>
+                <HeaderTitle
+                    title='Pull to refresh'
+                />
+                {
+                    data && <Text>{data}</Text>
+                }
+
+
+            </View>
+        </ScrollView>
+    )
 }
